4dc0ff8d5cab46c93b3f48b051b699c4e9c717cc
amandacorral07 <amandalewis0707@gmail.com>
2022-08-03 19:26:18 -0700

    Added bg video Sign Up

---
+++ src/components/auth/BackgroundVideo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../../style.css'
+
+
+const BackgroundVideo=()=>{
+return (
+    <>
+    <div class="overlay"></div>
+    <div id="bg-video">
+        <iframe width="599" height="337" src="https://www.youtube.com/embed/Jqf9haCd6mM" title="Dolomites Cinematic 4K Mavic Air 2 - Drone Video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+    </>
+)
+}
+
+export default BackgroundVideo

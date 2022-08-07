f6e2fb0b1979257d76089d87523fc08bbdbe4d76
jamestylladams <jamestylladams@gmail.com>
2022-08-07 14:21:30 -0600

    wprking dropdown with api search. need to assign list items data from api request

--- src/components/activities/SearchActivityModal.js
+++ src/components/activities/SearchActivityModal.js
@@ -1,32 +1,35 @@
 import React, { useState } from "react";
-import { Modal } from "react-bootstrap";
+import { DropdownButton, Modal } from "react-bootstrap";
 import ActivityForm from "../shared/ActivityForm";
 import { createActivity } from "../../api/activities";
 import Dropdown from "react-bootstrap/Dropdown";
-import { Form,Button,Container 
-} from 'react-bootstrap'
-import RecommendedList from './RecommendedList'
+
+import { Form, Button, Container } from "react-bootstrap";
+import RecommendedList from "./RecommendedList";
 import axios from "axios";
 let bussin = {
-    lat: 69,
-    lon: 69
-}
-
-
+  lat: 69,
+  lon: 69,
+};
 
 const SearchActivityModal = (props) => {
   const { user, destination, show, handleClose, msgAlert, triggerRefresh } =
     props;
-
+  const [value, setValue] = useState("");
+  const handleSelect = (e) => {
+    console.log(e);
+    setValue(e);
+  };
+  console.log(value);
   const [activity, setActivity] = useState({});
-  const [recommendedListShow, setRecommendedListShow] = useState(false)
+  const [recommendedListShow, setRecommendedListShow] = useState(false);
 
   const bussinFrFr = async () => {
-    console.log(Dropdown.Toggle)
+    console.log(Dropdown.Toggle);
     let places = await axios.get(
-      `https://api.tomtom.com/search/2/categorySearch/important%20tourist%20attraction.json?typeahead=true&lat=${destination.lat}&lon=${destination.lon}&view=Unified&relatedPois=off&key=9JyQb3r2IQDfXHOgwSTNBa8mkxAAuNAT`
+      `https://api.tomtom.com/search/2/categorySearch/${value}.json?typeahead=true&lat=${destination.lat}&lon=${destination.lon}&view=Unified&relatedPois=off&key=9JyQb3r2IQDfXHOgwSTNBa8mkxAAuNAT`
     );
-  
+
     console.log(places);
   };
 
@@ -76,40 +79,43 @@ const SearchActivityModal = (props) => {
   };
 
   return (
-    <Modal  show={show} onHide={handleClose}>
-        {<h3>Pick a Category</h3>}
-      <Modal.Header  closeButton /> 
+    <Modal show={show} onHide={handleClose}>
+      {<h3>Search {value}</h3>}
+      <Modal.Header closeButton />
       <Modal.Body>
-          <Form>
-              
-        <Dropdown>
+        <Form>
+          {/* <Dropdown>
           <Dropdown.Toggle variant="success" id="dropdown-basic">
             Dropdown Button
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">important tourist attraction</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">museum</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">parks</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">natural tourist attraction</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">statues</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">skateparks</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Amanda's rec: sushi</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">James' rec: mediterranean</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Dan's rec: breweries</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+          </Dropdown.Toggle> */}
+          <DropdownButton onSelect={handleSelect}>
+            <Dropdown.Item eventKey="important tourist attraction">
+              important tourist attraction
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="museum">museum</Dropdown.Item>
+            <Dropdown.Item eventKey="parks">parks</Dropdown.Item>
+            <Dropdown.Item eventKey="natural tourist attraction">
+              natural tourist attraction
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="statues">statues</Dropdown.Item>
+            <Dropdown.Item eventKey="skatepark">skateparks</Dropdown.Item>
+            <Dropdown.Item eventKey="sushi">sushi</Dropdown.Item>
+            <Dropdown.Item eventKey="mediterranean">
+              mediterranean
+            </Dropdown.Item>
+            <Dropdown.Item eventKey="breweries">breweries</Dropdown.Item>
+          </DropdownButton>
+          {/* </Dropdown> */}
         </Form>
         {user && destination.owner === user._id ? (
-              <>
-              <Button onClick={() => bussinFrFr()}>Submit</Button>
-
-              </>
-            ) : null}
+          <>
+            <Button onClick={() => bussinFrFr()}>Submit</Button>
+          </>
+        ) : null}
       </Modal.Body>
-      
+
       <RecommendedList
-  
+      //items={items}
       />
     </Modal>
   );

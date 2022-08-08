208029032cef2cd6ec3f885d1ed553c0f93336fd
danstacy1 <danstacy1@gmail.com>
2022-08-08 13:53:14 -0400

    added API call to edit destination button

--- src/components/destinations/EditDestinationModal.js
+++ src/components/destinations/EditDestinationModal.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 import { Modal } from 'react-bootstrap'
 import DestinationForm from '../shared/DestinationForm';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function EditDestinationModal(props) {
     const { user, show, handleClose, updateDestination, msgAlert, triggerRefresh } = props
 
     const [destination, setDestination] = useState(props.destination)
+
+    const navigate = useNavigate()
 
    
     const handleChange = (e) => {
@@ -26,10 +30,18 @@ export default function EditDestinationModal(props) {
         });
       };
 
-      const handleSubmit = (e) => {
+      const handleSubmit = async (e) => {
         e.preventDefault();
+        let testing = await axios.get(`https://api.opentripmap.com/0.1/en/places/geoname?name=${destination.name}&apikey=5ae2e3f221c38a28845f05b6ee69c6dabf1a1c065bbb25f00387dbd9`)
+        console.log('testing', testing)
+        destination.lat = testing.data.lat
+        destination.lon = testing.data.lon
+        destination.population = testing.data.population
+        console.log(destination)
+
         updateDestination(user, destination)
-        .then(() => handleClose())
+          .then(() => handleClose())
+          // .then(res => { navigate(`/destinations/${res.data.destination._id}`)})
           .then(() =>
             msgAlert({
               heading: "oh yea!",
